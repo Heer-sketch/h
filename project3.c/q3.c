c5574b371b3b7419b96327e4ece52a8859000716
unknown <tim@timrich.org>
2024-09-20 15:58:20 +0530

    first commit

---
+++ project3.c/q3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main ()
+{
+    int num, count=0 , first , last;
+
+    printf("Enter your value:-");
+    scanf("%d",&num);
+
+    last = num%10;
+
+    for (int i=1 ; num!=0 ; i++)
+     {
+        // printf("\n num %d",num);
+        first = num ;
+        num = num/10;
+        count++;
+     }
+
+     printf("\n First=%d,  last=%d",first, last);
+     printf("\n sum of total:%d",first+last);
+
+}
